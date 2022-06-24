0c06be595430c96742a3b82245807acf7565eb86
GailDev10 <GailDev10@gmail.com>
2022-06-24 16:17:58 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const app = express();  //some use server as the variable
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db
+dbConnectionString = process.env.DB_STRING,
+dbName = 'Recipes',
+collection
+
+MongoClient.connect( dbConnectionString )
+.then(client => {
+    console.log('Connected to Database')
+    db = client.db(dbName)
+    collection = db.collection('recipes')
+})
